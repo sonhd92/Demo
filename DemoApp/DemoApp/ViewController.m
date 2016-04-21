8868b11b0b49d0cf93d557a5fa4602bfaed614a2
khanhnq <khanhnq03@gmail.com>
2016-04-21 17:17:29 +0700

    New version

---
+++ DemoApp/DemoApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DemoApp
+//
+//  Created by khanh on 4/21/16.
+//  Copyright © 2016 KVip. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ DemoKit/DemoKit/DemoKit.h
@@ -0,0 +1,19 @@
+//
+//  DemoKit.h
+//  DemoKit
+//
+//  Created by khanh on 4/21/16.
+//  Copyright © 2016 KVip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DemoKit.
+FOUNDATION_EXPORT double DemoKitVersionNumber;
+
+//! Project version string for DemoKit.
+FOUNDATION_EXPORT const unsigned char DemoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoKit/PublicHeader.h>
+
+
