4fa04f266ca48c1f08c6f2cd0772b761675781f5
khanhnq <khanhnq03@gmail.com>
2016-04-22 09:59:08 +0700

    Sửa file Scrip để build

--- DemoKit/DemoKit/DemoKit.h
+++ DemoKit/DemoKit/DemoKit.h
@@ -7,13 +7,6 @@
 //
 
 #import <UIKit/UIKit.h>
-
-//! Project version number for DemoKit.
-FOUNDATION_EXPORT double DemoKitVersionNumber;
-
-//! Project version string for DemoKit.
-FOUNDATION_EXPORT const unsigned char DemoKitVersionString[];
-
 // In this header, you should import all the public headers of your framework using statements like #import <DemoKit/PublicHeader.h>
 
 
