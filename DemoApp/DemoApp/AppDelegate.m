18f27f48f8ba9dc6aec74465b06e7a6c39a95a91
Hoang Duc Son <sonhd.92@gmail.com>
2016-04-26 10:52:35 +0700

    no message

--- DemoApp/DemoApp/AppDelegate.m
+++ DemoApp/DemoApp/AppDelegate.m
@@ -21,6 +21,24 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [[KDemoDelegate sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
     [GIDSignIn sharedInstance].clientID = @"871137579127-9iq60cikpgnmhrchooedua9n0l0qou91.apps.googleusercontent.com";
+    
+    
+    NSString *currentVersion = [[UIDevice currentDevice] systemVersion];
+    if ([currentVersion compare:@"8.0" options:NSNumericSearch] == NSOrderedAscending)
+    {
+        UIRemoteNotificationType types =
+        UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert;
+        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:types];
+    }
+    else
+    {
+        UIUserNotificationType types =
+        UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert;
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+    }
+    
     return YES;
 }
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
@@ -35,5 +53,27 @@
                                     sourceApplication:sourceApplication
                                            annotation:annotation];
      }
+}
+
+-(void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo{
+    [KDemoDelegate handlePushNotification:userInfo application:application];
+}
+
+-(void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken{
+    NSString *deviceTokenStr = [NSString stringWithFormat:@"Device Token=%@", deviceToken];
+    
+    deviceTokenStr = [deviceTokenStr stringByReplacingOccurrencesOfString:@" " withString:@""];
+    deviceTokenStr = [deviceTokenStr stringByReplacingOccurrencesOfString:@"DeviceToken=<" withString:@""];
+    deviceTokenStr = [deviceTokenStr stringByReplacingOccurrencesOfString:@">" withString:@""];
+    [KDemoDelegate setDeviceToken:deviceTokenStr];
+}
+
+-(void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error{
+    NSLog(@"Failed with Error: %@", error);
+}
+
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
+{
+    [application registerForRemoteNotifications];
 }
 @end
